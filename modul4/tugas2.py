f3d86c4a7821d5953da43becddb3587875f135dc
sesiliafeb <146990259+sesiliafeb@users.noreply.github.com>
2023-10-19 22:17:44 +0700

    Add files via upload

--- modul4/tugas2.py
+++ modul4/tugas2.py
@@ -13,16 +13,6 @@ hasil = faktorial(nilai)
 print ("hasil faktorial :", hasil)
 
 
-# # program statis
-# def faktorial(n):
-#     if n == 0:
-#         return 1
-#     else:
-#         return n * faktorial(n-1)
-    
-# nilai = 10
 
-# hasil = faktorial(nilai)
-# print("hasil faktorial :", hasil)
 
 
