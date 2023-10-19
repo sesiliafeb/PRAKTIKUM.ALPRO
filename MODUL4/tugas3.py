9683938ae5015756fbe5e13da94433a0091f29c0
sesiliafeb <146990259+sesiliafeb@users.noreply.github.com>
2023-10-19 19:15:16 +0700

    Add files via upload

---
+++ MODUL4/tugas3.py
@@ -0,0 +1,12 @@
+
+#program dinamis
+bilangan = int(input("Masukkan bilangan: "))
+def pangkat ():
+    if pangkat == 0:
+        return 1
+    else:
+        return bilangan ** 2
+
+hasil = pangkat ()
+print(bilangan, "hasil pangkat :", hasil)
+
